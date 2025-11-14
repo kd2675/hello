ca1b5e14c0e3381fd7a0e9be49c68583882822bb
김도영 <zz01064842675@gmail.com>
2025-11-14 10:34:01 +0900

    Revert "feat: 문자열 덧셈 계산기 추가 – 입력 파싱, 검증, 계산 로직 구현 및 의존성 주입 완료"
    This reverts commit 82a6711fb1ff300e02ccf8c6adf4f8f46891e854.

--- main.go
+++ main.go
@@ -1,26 +1,18 @@
 package main
 
 import (
-	"hello/practice/chapter"
+	"hello/practice"
 )
 
 //TIP <p>To run your code, right-click the code and select <b>Run</b>.</p> <p>Alternatively, click
 // the <icon src="AllIcons.Actions.Execute"/> icon in the gutter and select the <b>Run</b> menu item from here.</p>
 
 func main() {
-	//chapter.Variable()
-	//chapter.DataType()
-	//chapter.Operator()
-	//chapter.Conditional()
-	//chapter.Loop()
-	//chapter.Function()
-	//chapter.AnonFunction()
-	//chapter.Closure()
-	//chapter.Slice()
-	//chapter.Map()
-	//chapter.Package()
-	//chapter.Struct()
-	//chapter.Interface()
-	//chapter.Error()
-	chapter.Catch()
+	practice.Variable()
+	practice.DataType()
+	practice.Operator()
+	practice.Conditional()
+	practice.Loop()
+	practice.Function()
+	practice.AnonFunction()
 }
--- practice/project/calculator/go.mod
+++
@@ -1,6 +0,0 @@
-module calculator
-
-go 1.21
-
-require (
-)
